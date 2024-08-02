f64d03da239057a62ae50b6a247fbccc3404dd3f
tinict <tinnguyensp.ict@gmail.com>
2024-08-02 16:09:12 +0700

    [breeze]: Setup laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-/**
- * @description: Route default, return view -> default 'welcome page'
- */
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-/**
- * @description: Home page of Zeal Quiz
- */
 Route::get('/', function () {
-    return view('app/page');
+    return ['Laravel' => app()->version()];
 });
 
-/**
- * @description: Home page of Zeal Quiz
- */
-Route::get('/profile', function () {
-    return view('app/profile/page');
-});
+require __DIR__.'/auth.php';
